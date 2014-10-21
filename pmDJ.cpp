57de613791faef9e75abbcc35b5ec7f81e593c3f
Chris Dusto <cricket_lover@live.com>
2014-10-21 14:17:45 -0700

    Usage instructions added in

--- pmDJ.cpp
+++ pmDJ.cpp
@@ -2,10 +2,18 @@
 #include "midiFile.hpp"
 #include "smdFile.hpp"
 #include "smdMidi.hpp"
+#include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
 int main(int argc, char *argv[]) {
+	if(argc<3) {
+		cout << "Usage: " << argv[0] << " <.smd file> [parameters] [<.smd file> [parameters] ...] <.mid file>\n";
+		cout 	<< "Parameters can be:\n"
+			<< "  -l# - Loop # times\n"
+			<< "  -t#:#.# - Trim #:#.# time off end of final loop\n";
+		return 0;
+	}
 	return 0;
 }

be58b1da803b06cc40448bb9c4aea939ca74327e
Chris Dusto <cricket_lover@live.com>
2014-10-21 14:13:51 -0700

    Heading and dummy main()

---
+++ pmDJ.cpp
@@ -0,0 +1,11 @@
+#include "instMap.hpp"
+#include "midiFile.hpp"
+#include "smdFile.hpp"
+#include "smdMidi.hpp"
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
